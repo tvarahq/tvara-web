30005ae00e76a229bbdde45770925abac70cf80d
Ashish Lal <ashishlal2003@gmail.com>
2025-09-01 15:53:16 +0530

    new tvara-app

---
+++ tvara-app/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div class="bg-background text-primary p-4">
+        This uses custom colors defined with CSS variables.
+      </div>
+    </>
+  );
+}
+
+export default App;
