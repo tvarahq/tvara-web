6aa7c86a371cf67780ead5979b9952364ab90dd8
Ashish Lal <ashishlal2003@gmail.com>
2025-09-01 22:12:10 +0530

    hero section almost done

--- tvara-app/src/App.jsx
+++ tvara-app/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
 
 function App() {
   return (
-    <>
-      <div class="bg-background text-primary p-4">
-        This uses custom colors defined with CSS variables.
-      </div>
-    </>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      {/* <Route path="/blog" element={<div>Blog Page</div>} />
+      <Route path="/docs" element={<div>Docs Page</div>} /> */}
+    </Routes>
   );
 }
 
